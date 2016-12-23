fb968eeedfa964e055fb154d28bca97ec7dbcd4b
Björn Fröhling <bjorn.frohling@qvik.fi>
2016-12-23 14:18:26 +0200

    Listing album titles

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -5,19 +5,26 @@ import {View, Text} from 'react-native';
 import axios from 'axios';
 
 class AlbumList extends Component {
+  state = { albums: [] };
 
 // life cycle method
   componentWillMount() {
     // Will be executed before component appears on screen
     axios.get('https://rallycoding.herokuapp.com/api/music_albums')
-    .then(response => console.log(response));
+    .then(response => this.setState({ albums: response.data}));
+  }
+
+  renderAlbums() {
+    return this.state.albums.map(album => <Text>{album.title}</Text>);
   }
 
   // Obligatory render method
   render() {
+    console.log(this.state);
+
     return (
       <View>
-        <Text>Album List!!!</Text>
+        {this.renderAlbums()}
         </View>
       );
     }
