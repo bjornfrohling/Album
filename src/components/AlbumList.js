469a175615dffd1da5c79f4ef5ec6d14efa10476
Björn Fröhling <bjorn.frohling@qvik.fi>
2017-01-09 17:32:13 +0200

    Added catch statement to fetch request

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -4,32 +4,34 @@ import React, {Component} from 'react';
 import {View, ScrollView} from 'react-native';
 import AlbumDetail from './AlbumDetail';
 
-
 class AlbumList extends Component {
-  state = { albums: [] };
-// life cycle method
+  // class level property
+  state = {
+    albums: []
+  };
+
+  // life cycle method
   componentWillMount() {
-// Will be executed before component appears on screen
+    // Will be executed before component appears on screen
 
-fetch('https://rallycoding.herokuapp.com/api/music_albums')
-.then((response) =>  {
-    return response.json() 
-  })
-.then((responseData) => { 
-   return responseData;
- })
-.then((data) => { 
-   console.log(data);
-   this.setState({ albums: data});
- });
-}
-
-  
+    fetch('https://rallycoding.herokuapp.com/api/music_albums').then((response) => {
+      return response.json()
+    }).then((responseData) => {
+      return responseData;
+    }).then((data) => {
+      console.log(data);
+      this.setState({albums: data});
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+  }
 
   renderAlbums() {
-    return this.state.albums.map(album =>
-      <AlbumDetail key={album.title} album={album} />
-    );
+    return this
+      .state
+      .albums
+      .map(album => <AlbumDetail key={album.title} album={album}/>);
   }
 
   // Obligatory render method
@@ -39,9 +41,9 @@ fetch('https://rallycoding.herokuapp.com/api/music_albums')
     return (
       <ScrollView style={styles.viewStyle}>
         {this.renderAlbums()}
-        </ScrollView>
-      );
-    }
+      </ScrollView>
+    );
+  }
 }
 
 const styles = {
